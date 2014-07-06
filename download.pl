436a74d65c81094eb939518ef3a2c7d6aca02383
Brian Hall <mrcodez@gmail.com>
2014-07-06 15:15:31 -0700

    downloading image code

--- download.pl
+++ download.pl
@@ -7,7 +7,8 @@ sub main {
 
 	print "Downloading...\n";
 	#print get("http://www.webcomic32.com");	
-	getstore("http://www.webcomic32.com", "home.html");
+	#getstore("http://www.webcomic32.com", "home.html");
+	getstore('http://i.imgur.com/Xw68Eib.png', 'cat.png');
 	print "Finished...\n";
 }
 
