7533b79bdd173f1f730346c15d04bf447eb744ca
Brian Hall <mrcodez@gmail.com>
2014-07-06 15:18:08 -0700

    download lesson final

--- download.pl
+++ download.pl
@@ -8,7 +8,14 @@ sub main {
 	print "Downloading...\n";
 	#print get("http://www.webcomic32.com");	
 	#getstore("http://www.webcomic32.com", "home.html");
-	getstore('http://i.imgur.com/Xw68Eib.png', 'cat.png');
+	my $code = getstore('http://i.imgur.com/Xw68Eib.png', 'cat.png');
+	
+	if($code == 200) {
+		print "Success!\n";
+	} else {
+		print "Failed.\n";
+	}
+	
 	print "Finished...\n";
 }
 
